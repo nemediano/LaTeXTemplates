d81aadde9ebbc4b7abc08051d501bfd42a50fd0c
nemediano <nemediano@gmail.com>
2019-11-10 20:07:34 -0800

    [Notes] Start of section on bit manipulation

--- Notes/src/bitManipulation.cpp
+++ Notes/src/bitManipulation.cpp
@@ -16,19 +16,37 @@ const size_t BITS_PER_BYTE = 8U;
 template<typename T>
 static std::string binStr(const T& x);
 
+template<typename T>
+static size_t bitSize(const T& x);
+
 int main(int argc, char* argv[]) {
-	unsigned char a = 1;
-	unsigned int b = 1;
+	unsigned char a = 0b101010;
+	unsigned int b = 9;
 	int c = 0xFF00FF08;
 
-	cout << "size of a: " << sizeof(a) * BITS_PER_BYTE << endl;
-	cout << "size of b: " << sizeof(b) * BITS_PER_BYTE << endl;
-	cout << "size of c: " << sizeof(c) * BITS_PER_BYTE << endl;
-	cout << "a = " << binStr(a) << endl;
-	cout << "b = " << binStr(b) << endl;
-	cout << "c = " << binStr(c) << endl;
+	cout << "size in bits of unsigned char: " << bitSize(a) << endl;
+	cout << "size in bits of unsigned int: " << bitSize(b) << endl;
+	cout << "size in bits of int: " << bitSize(c) << endl;
+	cout << "binary representation of a: " << binStr(a) << endl;
+	cout << "binary representation of b: " << binStr(b) << endl;
+	cout << "binary representation of c: " << binStr(c) << endl;
+
+  unsigned char d = a << 3;
+  unsigned char e = a >> 3;
+  unsigned char f = a >> 8;
+  unsigned char g = a << 8;
+  cout << "a: " << binStr(a) << " = " << int(a) << endl;
+  cout << "d: " << binStr(d) << endl;
+  cout << "e: " << binStr(e) << endl;
+  cout << "f: " << binStr(f) << endl;
+  cout << "g: " << binStr(g) << endl;
 
 	return EXIT_SUCCESS;
+}
+
+template<typename T>
+static size_t bitSize(const T& x) {
+  return sizeof(x) * BITS_PER_BYTE;
 }
 
 template<typename T>
