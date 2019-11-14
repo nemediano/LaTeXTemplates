ce8bdddf9efe4e0d85c9cb786bec31ac429c2b97
Jorge Garcia <jorgeg@nvidia.com>
2019-11-13 18:20:38 -0800

    [Notes] Add content about bit operations

--- Notes/src/bitManipulation.cpp
+++ Notes/src/bitManipulation.cpp
@@ -21,26 +21,54 @@ static size_t bitSize(const T& x);
 
 int main(int argc, char* argv[]) {
 
-	unsigned char a = 0b101010;
-	unsigned int b = 9;
-	int c = 0xFF00FF08;
+	unsigned char aChar = 0b101010;
+	unsigned int aUnsigInt = 9;
+	int anInt = 0xFF00FF08;
 
-	cout << "size in bits of unsigned char: " << bitSize(a) << endl;
-	cout << "size in bits of unsigned int: " << bitSize(b) << endl;
-	cout << "size in bits of int: " << bitSize(c) << endl;
-	cout << "binary representation of a: " << binStr(a) << endl;
-	cout << "binary representation of b: " << binStr(b) << endl;
-	cout << "binary representation of c: " << binStr(c) << endl;
+	cout << "size in bits of unsigned char: " << bitSize(aChar) << endl;
+	cout << "size in bits of unsigned int: " << bitSize(aUnsigInt) << endl;
+	cout << "size in bits of int: " << bitSize(anInt) << endl;
+	cout << "binary representation of aChar: " << binStr(aChar) << endl;
+	cout << "binary representation of aUnsigInt: " << binStr(aUnsigInt) << endl;
+	cout << "binary representation of anInt: " << binStr(anInt) << endl;
 
-  unsigned char d = a << 3;
-  unsigned char e = a >> 3;
-  unsigned char f = a >> bitSize(a);
-  unsigned char g = a << bitSize(a);
+  cout << endl << "Bit shifting sample" << endl;
+  unsigned char a = aChar;
+  unsigned char b = a << 3;
+  unsigned char c = a >> 3;
+  unsigned char d = a >> bitSize(a);
+  unsigned char e = a << bitSize(a);
+  unsigned char f = a;
+
   cout << "a: " << binStr(a) << " = " << int(a) << endl;
+  cout << "b: " << binStr(b) << " = " << int(b) << endl;
+  cout << "c: " << binStr(c) << " = " << int(c) << endl;
   cout << "d: " << binStr(d) << " = " << int(d) << endl;
   cout << "e: " << binStr(e) << " = " << int(e) << endl;
-  cout << "f: " << binStr(f) << " = " << int(f) << endl;
-  cout << "g: " << binStr(g) << " = " << int(g) << endl;
+
+  cout << endl << "Bitwise binary operations sample" << endl;
+  a = 0b10101101;
+  b = 0b11100110;
+  c = a & b;
+  d = a | b;
+  e = a ^ b;
+  f = ~a;
+
+  cout << "c = a & b" << endl;
+  cout << "a: " << binStr(a) << endl;
+  cout << "b: " << binStr(b) << endl;
+  cout << "c: " << binStr(c) << endl;
+  cout << "d = a | b" << endl;
+  cout << "a: " << binStr(a) << endl;
+  cout << "b: " << binStr(b) << endl;
+  cout << "d: " << binStr(d) << endl;
+  cout << "e = a ^ b" << endl;
+  cout << "a: " << binStr(a) << endl;
+  cout << "b: " << binStr(b) << endl;
+  cout << "e: " << binStr(e) << endl;
+  cout << "f = ~a" << endl;
+  cout << "a: " << binStr(a) << endl;
+  cout << "f: " << binStr(f) << endl;
 
 	return EXIT_SUCCESS;
 }
